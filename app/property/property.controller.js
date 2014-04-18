ad4491e8aa837ee1efc381a513aac21604e561ca
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2014-04-18 16:54:50 +0200

    added the current date function

--- app/property/property.controller.js
+++ app/property/property.controller.js
@@ -29,3 +29,14 @@ function PropertyFormCtrl($scope) {
       }
     };
 }
+
+function GetCurrentDate() {
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth()+1;
+	if (month<10){
+		month="0" + month;
+	};
+	var day = date.getDate();
+	$scope.date = year + "-" + month + "-" + day;
+}
